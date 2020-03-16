82f55cc9270d149d0977ec211c355b7e24c80fc3
Wannoko <rp.kazuya.hosokawa@gmail.com>
2020-03-16 17:01:06 +0900

    init

---
+++ WebViewScrollHideButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewScrollHideButton
+//
+//  Created by 細川和也 on 2020/03/16.
+//  Copyright © 2020 wannoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
